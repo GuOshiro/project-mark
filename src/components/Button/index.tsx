51f279713f7bc9c53037019e3ab2cce288148f24
Gustavo Oshiro <gustavoaoshiro@gmail.com>
2023-07-21 11:36:44 -0300

    feat - create button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,19 @@
+export default function Button({
+  text,
+  action,
+  type = 'button'
+}: {
+  text: string;
+  action?: () => void;
+  type?: 'submit' | 'reset' | 'button' | undefined;
+}) {
+  return (
+    <button
+      type={type}
+      onClick={action}
+      className="rounded-full bg-blue-highlight px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    >
+      {text}
+    </button>
+  );
+}
