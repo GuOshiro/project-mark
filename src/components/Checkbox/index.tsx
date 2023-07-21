94a3c6eb250379bbe0aada33c486c2fa7f23e456
Gustavo Oshiro <gustavoaoshiro@gmail.com>
2023-07-21 12:42:02 -0300

    feat - create checkbox

---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,37 @@
+export default function Checkbox({
+  title,
+  checked,
+  selected,
+}: {
+  title?: string;
+  checked?: boolean;
+  selected?: boolean;
+}) {
+  return (
+    <div
+      className={`mx-2 relative flex items-start border-solid border-2 rounded-2xl border-gray-300 w-52 p-4 hover:bg-blue-light bg-white ease-in duration-200 cursor-pointer `}
+    >
+      {selected && (
+        <div className="absolute bottom-[-7px] right-[45%] ml-2 -mb-[1px] inline-block overflow-hidden border-gray-300 hover:bg-blue-light bg-white ease-in duration-200 cursor-pointer">
+          <div className="border-solid border-t-black border-t-0 border-x-transparent border-x-8 border-b-0"></div>
+          <div className="border-solid border-t-gray-300 border-t-8 border-x-transparent border-x-8 border-b-0"></div>
+        </div>
+      )}
+      <div className="flex h-6 items-center">
+        <input
+          id="comments"
+          aria-describedby="comments-description"
+          name="comments"
+          type="checkbox"
+          checked={checked}
+          className="h-4 w-4 rounded border-gray-300 "
+        />
+      </div>
+      <div className="ml-3 text-sm leading-6">
+        <label htmlFor="comments" className="font-medium text-gray-800 text-sm">
+          {title}
+        </label>
+      </div>
+    </div>
+  );
+}
--- src/containers/onboarding/index.tsx
+++ src/containers/onboarding/index.tsx
@@ -1,10 +1,37 @@
+import Checkbox from "@/components/Checkbox";
+
 export default function Onboarding({}) {
+  const checkboxList = [
+    {
+      title: "Basic",
+      checked: true,
+      selected: true,
+    },
+    {
+      title: "Advanced",
+    },
+    {
+      title: "Expert",
+    },
+    {
+      title: "Custom",
+    },
+  ];
   return (
-    <div className="flex items-center justify-center py-10">
-      <h1 className="text-2xl font-bold text-gray-800">
+    <div className="flex flex-col items-center justify-center py-10">
+      <h1 className="text-3xl font-bold text-gray-800">
         Please select the type of sales pipeline that best fits to you company
       </h1>
-
+      <div className="flex flex-row items-center justify-center py-10">
+        {checkboxList.map((item) => (
+          <Checkbox
+            key={item.title}
+            title={item.title}
+            checked={item.checked}
+            selected={item.selected}
+          />
+        ))}
+      </div>
     </div>
   );
 }
